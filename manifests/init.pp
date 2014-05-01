b3332447edf4f65c049c231d53fcb65a21ca0083
Thomas Foster <tfoster@promnetwork.com>
2014-05-01 10:07:57 -0400

    Added sample init.pp file

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,2 +1,11 @@
 class rhnreg_ks {
+        rhn_register { 'title':
+        activationkeys => 'satellite-key',
+        ensure         => 'present or absent',
+        username       => 'user to add/delte server accounts',
+        password       => 'password for user',
+        profile_name   => 'This will setup how the server is named in the satellite server or it will use `hostname`',
+        server_url     => 'server url up to the XMLRPC',
+        #force         => 'set to true or false if you want to force an update every run',
+        }
 }
