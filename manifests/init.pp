fb0b577ca6ef19d2e012fc366be345125242f72d
Thomas Foster <tfoster@promnetwork.com>
2014-05-02 19:07:50 -0400

    test init.pp file

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,35 @@
 class rhnreg_ks {
-        rhn_register { 'title':
-        activationkeys => 'satellite-key',
-        ensure         => 'present or absent',
-        username       => 'user to add/delte server accounts',
-        password       => 'password for user',
-        profile_name   => 'This will setup how the server is named in the satellite server or it will use `hostname`',
-        server_url     => 'server url up to the XMLRPC',
-        #force         => 'set to true or false if you want to force an update every run',
+     
+        file { "checksat":
+          ensure => file,
+          path => '/usr/local/bin/checksat',
+          mode => 0755,
+          owner => 'root',
+          group => 'root',
+          source => 'puppet:///modules/rhnreg_ks/checksat',
+          before => Rhn_register['dev1foster'],
+        }         
+
+        file { "unregister":
+          ensure => file,
+          path => '/usr/local/bin/unregister',
+          mode => 0755,
+          owner => 'root',
+          group => 'root',
+          source => 'puppet:///modules/rhnreg_ks/unregister',
+          before => Rhn_register['dev1foster'],
+        }         
+
+          
+        rhn_register { 'dev1foster':
+         activationkeys => '2-rhel6-key',                                                                                                                  
+         ensure         => 'present',                                                                                        
+         username       => 'syseng_admin',                                                                                                                  
+         password       => '4u2use2',                                                                                                                                                                    
+         #profile_name   => 'dev1foster.promnetwork.com',                                                                                                                                                  
+         server_url     => 'https://vasat.promnetwork.com/XMLRPC',                                                                                                                           
+         #force         => true,                                                                                                                                                                       
+         require        => [ File["checksat"], File["unregister"] ]
         }
 }
+
