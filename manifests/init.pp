38f3b78def5988bd8346ac883216f32c672e9552
Thomas Foster <thomas.foster80@gmail.com>
2014-05-03 09:21:28 -0400

    added sample manifest file

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,12 @@
 class rhnreg_ks {
         rhn_register { 'title':
-        activationkeys => 'satellite-key',
-        ensure         => 'present or absent',
-        username       => 'user to add/delte server accounts',
-        password       => 'password for user',
-        profile_name   => 'This will setup how the server is named in the satellite server or it will use `hostname`',
-        server_url     => 'server url up to the XMLRPC',
-        #force         => 'set to true or false if you want to force an update every run',
+         activationkeys => 'activationkey',                                                                                                                  
+         ensure         => 'present',                                                                                        
+         username       => 'username',                                                                                                                  
+         password       => 'password',                                                                                                                                                            
+         profile_name   => 'profilename',                                                                                                                                        
+         server_url     => 'serverurl/XMLRPC',                                                                                                                           
+         #force         => true,                                                                                                                                                                    
         }
 }
+
