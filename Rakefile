870c07e4521cbcd8ad09a7272419cee5450cdaa9
Thomas Foster <thomas.foster80@gmail.com>
2014-05-04 14:32:37 -0400

    add checks for travis

--- Rakefile
+++ Rakefile
@@ -1,6 +1,6 @@
 require 'rake'
 require 'rubygems'
-require 'puppetlabs_spec_helper/rake_tasks'
+#require 'puppetlabs_spec_helper/rake_tasks'
 
 require 'rspec/core/rake_task'
 require 'puppet-lint/tasks/puppet-lint'
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,12 @@
 class rhnreg_ks {
-        rhn_register { 'title':
-         activationkeys => 'activationkey',                                                                                                                  
-         ensure         => 'present',                                                                                        
-         username       => 'username',                                                                                                                  
-         password       => 'password',                                                                                                                                                            
-         profile_name   => 'profilename',                                                                                                                                        
-         server_url     => 'serverurl/XMLRPC',                                                                                                                           
-         #force         => true,                                                                                                                                                                    
-        }
+  rhn_register { 'title':
+  ensure         => 'present',
+  activationkeys => 'activationkey',
+  username       => 'username',
+  password       => 'password',
+  profile_name   => 'profilename',
+  server_url     => 'serverurl/XMLRPC',
+  #force         => true,
+  }
 }
 
